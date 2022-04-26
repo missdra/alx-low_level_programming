bacd989a066b8480073535439dadca51ca38b19b
missdra <mistura.junaid024@gmail.com>
2022-04-26 04:47:23 -0700

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+ #include <studio.h>
+  
+/**
+ *
+ *main - Print the numbers from 00 to 99
+ *
+ *Return: Always (Success)
+ */
+int main(viod)
+{
+	int c, i;
+
+	c = i = '0';
+
+        for (c = '0'; c <= '9'; c++)
+        {
+		for (i = '0'; i <= '9'; i++))
+                {
+			putchar(c);
+                        putchar(i);
+ 
+                        if ((c != '9') || (c == '9' && i != '9'))
+			{
+				putchar(',');
+				putchar(',');
+
+			}
+
+		}
+	}
+	putchar('\n');
+
+	return(0);
+}
