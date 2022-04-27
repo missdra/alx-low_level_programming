5784ab2949036ce3a303359f475a52fca91183b3
missdra <mistura.junaid024@gmail.com>
2022-04-27 09:19:35 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,15 @@
- #include <studio.h>
-  
-/**
- * main - Print the numbers from 00 to 99
- *
- * Return: Always (Success)
- */
-int main(viod)
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
 {
-	int c, i;
+	int n;
 
-	c = i = '0';
-
-        for (c = '0'; c <= '9'; c++)
-        {
-		for (i = '0'; i <= '9'; i++))
-                {
-			putchar(c);
-                        putchar(i);
- 
-                        if ((c != '9') || (c == '9' && i != '9'))
-			{
-				putchar(',');
-				putchar(',');
-
-			}
-
-		}
-	}
-
-	putchar('\n');
-
-	return(0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
 }
